5b91de9d8f0ba5cba65218c85548f48eaee5cf75
Burak Aykanat <burak.aykanat12@gmail.com>
2017-09-15 21:29:39 +0300

    Add files via upload

---
+++ AngularJsHttp/app.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module("AngularJs-Http",[]);
+
+app.controller("Controller", function ($scope, $http){
+
+    var url = "https://jsonplaceholder.typicode.com/albums";
+
+    $http.get(url).then(function successCallBack(response) {
+
+        $scope.users = response.data;
+
+    }, function errorCallBack(response) {
+        alert("Request not provided ...")
+    });
+
+    $scope.SearchId = function () {
+        $scope.IdResult = $scope.users.filter(function (item) {
+            return item.id == $scope.Id;
+        });
+
+        $scope.Id = "";
+
+        if (!$scope.IdResult.length) {
+            alert("Id number was not found.");
+        }
+    }
+});
